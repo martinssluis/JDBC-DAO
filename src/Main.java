46265e5976e3d7cfa95226a71c7aa22c3b4499b1
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-02 15:31:35 -0300

    feat(Department): add Department entity class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+package src;
+
+import src.model.entites.Department;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
 }
