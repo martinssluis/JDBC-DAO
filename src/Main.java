82b3043a337c32c1d33823901879492f4528a73a
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-02 16:01:45 -0300

    feat(Seller): create Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 package src;
 
 import src.model.entites.Department;
+import src.model.entites.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
+
     }
 }
