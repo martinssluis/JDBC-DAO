33ff0695b339d64ee3e06f2a3db5d7d96ca92acc
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-02 18:04:26 -0300

    feat(DaoFactory): add DaoFactory content

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,4 +1,10 @@
 package src.model.dao;
 
+import src.model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
 }
