8058869fe72e54c201718167008eb133a4785acf
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-02 18:42:04 -0300

    fix(SellerDao): fixes Department findById to Seller findById

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,6 +1,5 @@
 package src.model.dao;
 
-import src.model.entites.Department;
 import src.model.entites.Seller;
 
 import java.util.List;
@@ -10,6 +9,6 @@ public interface SellerDao {
     void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Integer id);
-    Department findById(Integer id);
+    Seller findById(Integer id);
     List<Seller> findAll();
 }
