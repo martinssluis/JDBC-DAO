f789a3a5540fa064526a3ed7d0aeb5559581bd1c
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-04 09:55:49 -0300

    feat: reusing instantiation

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,12 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller2 = sellerDao.findById(3);
+        System.out.println("=== TEST 1; seller findById ===");
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(obj);
         System.out.println(seller);
-        System.out.println(seller2);
 
     }
 }
