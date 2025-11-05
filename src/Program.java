10692aa81a0dd3bb8a6ba370a1593af36f8667df
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-05 13:56:33 -0300

    feat(Program.java): add entry point to run Department instances

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,20 @@
 package src;
 
+import src.model.dao.DaoFactory;
+import src.model.dao.DepartmentDao;
+import src.model.entites.Department;
+
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args){
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("\n=== TEST 1: department insert ===");
+        Department musicDepartment = new Department(7, "Music");
+        departmentDao.insert(musicDepartment);
+        System.out.println("Inserted! New id = " + musicDepartment.getId());
+
+    }
 }
