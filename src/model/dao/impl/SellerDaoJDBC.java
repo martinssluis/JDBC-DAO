5433c6187bbe136aeed0977160f9a3c82a94837a
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-05 12:53:26 -0300

    feat(seller): validation of seller id deletion.

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -91,8 +91,12 @@ public class SellerDaoJDBC implements SellerDao {
             st = conn.prepareStatement(
                     "DELETE FROM seller WHERE Id = ?");
 
-            st.setInt(1,id);
-            st.executeUpdate();
+          st.setInt(1,id);
+
+          int rows =  st.executeUpdate();
+          if (rows == 0){
+              throw new SQLException();
+          }
 
         } catch (SQLException e){
             throw new DbException(e.getMessage());
