0f469bee04fd2e2b66962c3705972435c42732a6
Luis Martins <luishenrique.martinslima@gmail.com>
2025-11-05 13:51:40 -0300

    feat(DaoFactory.java): create a DepartmentDao Factory

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,14 @@
 package src.model.dao;
 
 import db.DB;
+import src.model.dao.impl.DepartmentDaoJDBC;
 import src.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+    public static DepartmentDao createDepartmentDao(){return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
